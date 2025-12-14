3dfd2b44865db26d508ab888656a9e382cf0f516
Eren <e.basaran2004@gmail.com>
2025-12-14 03:15:29 +0100

    fix: bad performance after going to a next or previous page

--- composeApp/src/androidMain/kotlin/org/example/project/Composables/Standard/Read/viewer/webtoon/WebtoonReader.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/Composables/Standard/Read/viewer/webtoon/WebtoonReader.kt
@@ -21,23 +21,17 @@ fun WebtoonReader(
     AndroidView<RecyclerView>(
         modifier = modifier,
         factory = { context: Context ->
-            // Preload all images into cache
             images.forEach { url ->
                 val req = buildImageRequest(context, url, headers)
                 imageLoader.enqueue(req)
             }
 
             RecyclerView(context).apply {
-
-                // This is so it doesn't load in real time when scrolling
-                // This can also causes performance issues, I am currently not sure what the best approach would be
-                // TODO: Find a better solution
                 layoutManager =
                     LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false).apply {
-                        isItemPrefetchEnabled = false // Prevent lazy loading
+                        isItemPrefetchEnabled = false
                     }
                 setHasFixedSize(true)
-                setItemViewCacheSize(images.size)
                 setItemViewCacheSize(images.size)
 
                 adapter = WebtoonRecyclerAdapter(
@@ -49,13 +43,18 @@ fun WebtoonReader(
                 )
                 recycledViewPool.setMaxRecycledViews(0, 0)
 
-
                 translationZ = 20f
                 elevation = 20f
                 bringToFront()
             }
         },
         update = { recyclerView: RecyclerView ->
+            // Clear memory cache to release old images
+            imageLoader.memoryCache?.clear()
+
+            // Swap adapter with null first to force cleanup
+            recyclerView.swapAdapter(null, true)
+
             recyclerView.adapter = WebtoonRecyclerAdapter(
                 images,
                 headers,
@@ -64,17 +63,16 @@ fun WebtoonReader(
                 onNextChapter
             )
 
-            // Preload new images
             images.forEach { url ->
                 val req = buildImageRequest(recyclerView.context, url, headers)
                 imageLoader.enqueue(req)
             }
 
             recyclerView.setItemViewCacheSize(images.size)
-            recyclerView.recycledViewPool.setMaxRecycledViews(0, 0)
-            (recyclerView.layoutManager as? LinearLayoutManager)?.isItemPrefetchEnabled = false
-
+            recyclerView.recycledViewPool.clear()
             recyclerView.scrollToPosition(0)
+
+            recyclerView.requestLayout()
         }
     )
 }
