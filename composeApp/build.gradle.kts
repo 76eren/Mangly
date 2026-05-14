b64219174af8a5178bd9f740895d38f529729403
Eren <e.basaran2004@gmail.com>
2026-05-14 23:43:33 +0200

    fix: bump up app version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "com.eren76.mangly"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
     }
     packaging {
         resources {
