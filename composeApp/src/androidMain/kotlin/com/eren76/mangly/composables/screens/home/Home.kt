5667e60f4093590af8a77ad00c7b6cb115a613c8
Eren <e.basaran2004@gmail.com>
2026-07-06 14:31:03 +0200

    fix: make downloads list follow the home sorting

--- composeApp/src/androidMain/kotlin/com/eren76/mangly/composables/screens/home/Home.kt
+++ composeApp/src/androidMain/kotlin/com/eren76/mangly/composables/screens/home/Home.kt
@@ -53,10 +53,16 @@ fun Home(
     }
 
     val favorites = favoritesViewModel.favorites.value
-    val sortedFavorites = remember(favorites, sorting) {
+    val downloads = downloadsViewModel.downloads.value
+    val historyWithChapters = historyViewModel.historyWithChapters.value
+
+    val sortedFavorites = remember(favorites, sorting, historyWithChapters) {
         sortFavorites(favorites, sorting, historyViewModel)
     }
-    val downloads = downloadsViewModel.downloads.value
+    val sortedDownloads = remember(downloads, sorting, historyWithChapters) {
+        sortDownloads(downloads, sorting, historyViewModel)
+    }
+
     val isPaginationEnabled: Boolean =
         paginationPreferences.getBoolean(Constants.PAGINATION_ENABLED_KEY, false)
     val pageSize: Int = paginationPreferences.getInt(
@@ -79,7 +85,7 @@ fun Home(
 
         // I kinda hate this but for now it works, needs to be refactored at some point
         if (showDownloads) {
-            if (downloads.isEmpty()) {
+            if (sortedDownloads.isEmpty()) {
                 Text(
                     text = "No downloads yet.",
                     style = MaterialTheme.typography.bodyMedium,
@@ -88,7 +94,7 @@ fun Home(
             } else {
                 if (!isPaginationEnabled) {
                     ShowDownloadsInLazyGrid(
-                        downloads = downloads,
+                        downloads = sortedDownloads,
                         extensionMetadataViewModel = extensionMetadataViewModel,
                         navHostController = navHostController,
                         downloadsViewModel = downloadsViewModel
@@ -96,7 +102,7 @@ fun Home(
                 } else {
                     PaginatedDownloads(
                         pageSize = pageSize,
-                        downloads = downloads,
+                        downloads = sortedDownloads,
                         extensionMetadataViewModel = extensionMetadataViewModel,
                         downloadsViewModel = downloadsViewModel,
                         navHostController = navHostController
